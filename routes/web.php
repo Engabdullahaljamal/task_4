577cfc6df0db457386b3447aac2d71e9a9618a1d
abdullah <en>
2024-04-13 21:00:23 +0300

    new update

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::get('/view-employee', [App\Http\Controllers\EmployeeController::class, 'view_employee'])->name('employee.view');
 Route::get('/create-employee', [App\Http\Controllers\EmployeeController::class, 'create_employee'])->name('employee.create')->middleware('admin');
 Route::post('/save-employee', [App\Http\Controllers\EmployeeController::class, 'save_employee'])->name('employee.save');
-Route::get('/edit-employee/{id}', [App\Http\Controllers\EmployeeController::class, 'edit_employee'])->name('employee.edit');
+Route::get('/edit-employee/{id}', [App\Http\Controllers\EmployeeController::class, 'edit_employee'])->name('employee.edit')->middleware('admin');;
 Route::put('/update-employee/{id}', [App\Http\Controllers\EmployeeController::class, 'update_employee'])->name('employee.update');
-Route::get('/delete-employee/{id}', [App\Http\Controllers\EmployeeController::class, 'delete_employee'])->name('employee.delete');
+Route::get('/delete-employee/{id}', [App\Http\Controllers\EmployeeController::class, 'delete_employee'])->name('employee.delete')->middleware('admin');;
 //Route::post('/save-employee', [App\Http\Controllers\Auth\RegisterController::class, 'create'])->name('employee.save');
